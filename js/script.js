f8b06e62600383975a826da90426f3fab68d82af
pilar-psico <160405011+pilar-psico@users.noreply.github.com>
2024-03-11 16:35:28 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 function abrirNav(){
-    document.getElementById("menuOculto").style.width="150px";
-    document.getElementById("principal").style.marginRight="150px";
+    document.getElementById("menuOculto").style.width="180px";
+    document.getElementById("principal").style.marginRight="180px";
 }
 function fecharNav(){
     document.getElementById("menuOculto").style.width="0";
     document.getElementById("principal").style.marginRight="0";
+}
+
+function toggleMode() {
+    const html = document.documentElement
+        html.classList.toggle("light");
+
 }
